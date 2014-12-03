1f90ea2c8a065be02b4760a801db1a0fa5482b05
barbara rojas <brojasflores@hotmail.com>
2014-12-03 19:51:56 -0300

    bonito

---
+++ practica/app/controllers/loginController.php
@@ -0,0 +1,53 @@
+<?php
+
+class loginController extends BaseController {
+
+
+	protected $layout='layouts.master';
+
+	public function getIndex()
+	{
+		return $this->layout->content = View::make('login.login');
+	}
+
+	public function post_index()
+	{
+        	$userdata = array(
+            'id' => Input::get('rut'),
+            'rut' => Input::get('rut'),
+            'password' => Input::get('password')
+        );
+
+        $rules = array(
+            'rut' => 'Required',
+            'password' => 'Required'
+        );
+
+        if (Auth::attempt($userdata)) {
+                // Redirect to homepage
+                return "hola";
+        }
+        else
+        {
+        	//return "no hola";
+        	//return View::make("Alumnos.add");
+            return Redirect::to('login')->with('login_errors', true);
+        }
+        
+
+	}
+
+
+	public function showProfesor()
+	{
+		return $this->layout->content = View::make('test.profesor');
+	}
+
+
+		public function showAlumno()
+	{
+		return $this->layout->content = View::make('test.alumno');
+	}
+
+
+}
