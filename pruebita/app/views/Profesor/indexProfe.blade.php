23862b3957b79c632b302bcfb2612dce4d9c5ed7
barbara rojas <brojasflores@hotmail.com>
2014-12-05 19:18:27 -0300

    ensayo

---
+++ pruebita/app/views/Profesor/indexProfe.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.master')
+
+@section('titulo')
+    UTEM - Bienvenido
+@stop 
+
+@section('sidebar')
+
+<div id="wrapper">
+<nav class="navbar navbar-fixed-top navbar-inverse" role="navigation"  >
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="./">Administrador de Pautas y Tareas</a>
+        </div>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="./">Inicio</a></li>
+      </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="/login">Ingresar al Portal<span class="sr-only">(current)</span></a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Reports</a></li>
+            <li><a href="#">Analytics</a></li>
+            <li><a href="#">Export</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item</a></li>
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+            <li><a href="">More navigation</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+          </ul>
+        </div>
+
+       
+
+          
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  
+
+<div id="global-zeroclipboard-html-bridge" class="global-zeroclipboard-container" style="position: absolute; left: 0px; top: -9999px; width: 15px; height: 15px; z-index: 999999999;" title="" data-original-title="Copy to clipboard">      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" id="global-zeroclipboard-flash-bridge" width="100%" height="100%">         <param name="movie" value="/assets/flash/ZeroClipboard.swf?noCache=1417806659736">         <param name="allowScriptAccess" value="sameDomain">         <param name="scale" value="exactfit">         <param name="loop" value="false">         <param name="menu" value="false">         <param name="quality" value="best">         <param name="bgcolor" value="#ffffff">         <param name="wmode" value="transparent">         <param name="flashvars" value="trustedOrigins=getbootstrap.com%2C%2F%2Fgetbootstrap.com%2Chttp%3A%2F%2Fgetbootstrap.com">         <embed src="/assets/flash/ZeroClipboard.swf?noCache=1417806659736" loop="false" menu="false" quality="best" bgcolor="#ffffff" width="100%" height="100%" name="global-zeroclipboard-flash-bridge" allowscriptaccess="sameDomain" allowfullscreen="false" type="application/x-shockwave-flash" wmode="transparent" pluginspage="http://www.macromedia.com/go/getflashplayer" flashvars="trustedOrigins=getbootstrap.com%2C%2F%2Fgetbootstrap.com%2Chttp%3A%2F%2Fgetbootstrap.com" scale="exactfit">                </object></div><svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200" preserveAspectRatio="none" style="visibility: hidden; position: absolute; top: -100%; left: -100%;"><defs></defs><text x="0" y="10" style="font-weight:bold;font-size:10pt;font-family:Arial, Helvetica, Open Sans, sans-serif;dominant-baseline:middle">200x200</text></svg></body>
+@stop
+
+@section('content')
+
+
+<br>
+<br>
+
+                        
+                        <div class="panel-heading">
+                            
+                            <h2><b>Bienvenido Profesor  </b><i class="glyphicon glyphicon-folder-open" aria-hidden="true"> </i></h2> 
+
+                        </div>
+                        <div class="panel-body">
+                          
+              
+                            <p>
+                            
+                              <b> Este es su panel de administración de su sitio web, en el cual
+                                  usted podrá agregar, editar y eliminar archivos.</b> 
+                                
+                            </p>
+                            <hr>
+
+
+  <div class="form-group">
+    <label for="ejemplo_archivo_1">Adjuntar un archivo</label>
+    <input type="file" id="ejemplo_archivo_1">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+                            </div>
+
+                        </div>
+                 
+
+
+@stop
